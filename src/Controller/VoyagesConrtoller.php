fa720a6b75ae3651cf81f4a2c9d0da8b3aa559e7
hillel magnichewer <h.magnichewer@gmail.com>
2023-11-05 15:29:32 +0100

    "Phase 10 : ORM (filtrage des enregistrements).".

--- src/Controller/VoyagesConrtoller.php
+++ src/Controller/VoyagesConrtoller.php
@@ -5,6 +5,7 @@ use App\Repository\VisiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Description of VoyagesConrtoller
@@ -45,5 +46,20 @@ class VoyagesConrtoller extends AbstractController{
         return $this->render("pages/voyages.html.twig", [
             'visites' => $visites
                 ]);
+    }
+    
+    /**
+     * @Route("/voyages/recherche/{champ}", name="voyages.findallequal")
+     * @param type $champ
+     * @param Request $request
+     * @return Response 
+     */
+    public function findAllEqual($champ, Request $request):Response {
+        $valeur = $request->get("recherche");
+        $visites = $this->repository->findByEqualValue($champ, $valeur);
+        return $this->render("pages/voyages.html.twig", [
+            'visites' => $visites
+        ]);
+        
     }
 }
